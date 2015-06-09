451813a4d03833ba652f43d7cb90f4e26dc75728
Sean Mikalson <sean.mikalson@gmail.com>
2015-06-08 23:50:35 -0600

    deploy

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ var bodyParser = require('body-parser');
 var pg = require('pg');
 var app = express();
 
-app.use(bodyParser.json({limit:'500kb'}));
+app.use(bodyParser.json({limit:'100000kb'}));
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -25,7 +25,7 @@ app.use(function (req, res, next) {
 
 app.post('/', function (req, res) {
     console.log('posting picture');
-    pg.connect('postgres://lostape:Filosoft02@localhost/foto', function(err, client, done){
+    pg.connect('postgres://ogtsopfqqoevzt:SEaLj1YCxRwaaKf8jwq5F_P_5M@ec2-23-23-199-181.compute-1.amazonaws.com:5432/d3d7klm95tukvg', function(err, client, done){
         if(!err) {
             client.query('insert into pictures (data) values(\''+req.body.data+'\')', function(err, results) {
                 console.log(err);
@@ -38,7 +38,7 @@ app.post('/', function (req, res) {
 });
 
 app.get('/', function(req, res) {
-    pg.connect('postgres://lostape:Filosoft02@localhost/foto', function(err, client, done){
+    pg.connect('postgres://ogtsopfqqoevzt:SEaLj1YCxRwaaKf8jwq5F_P_5M@ec2-23-23-199-181.compute-1.amazonaws.com:5432/d3d7klm95tukvg', function(err, client, done){
         if(!err) {
             client.query('select * from pictures', function(err, results) {
                 done();
@@ -48,7 +48,7 @@ app.get('/', function(req, res) {
     });
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 80, function () {
 
     var host = server.address().address;
     var port = server.address().port;
